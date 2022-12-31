0dea581351472b6a30745fd2546b70976a24d5de
Jeferson Silva <jeferson.i.silva@gmail.com>
2022-12-31 05:51:02 -0300

    deploy

--- docs/_nuxt/composables.a2a1043c.js
+++
@@ -1,1 +0,0 @@
-import{J as r}from"./entry.b0f1af28.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
---
+++ docs/_nuxt/composables.ca9cb2e0.js
@@ -0,0 +1,1 @@
+import{G as r}from"./entry.032fc9ef.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
