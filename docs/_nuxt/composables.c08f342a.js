fb04872c4b9a6cff9ea90a1d98c78732b7653735
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-01-16 17:37:32 -0300

    build

---
+++ docs/_nuxt/composables.c08f342a.js
@@ -0,0 +1,1 @@
+import{H as r}from"./entry.e1aa9ba1.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
---
+++ docs/_nuxt/index.a1d33917.js
@@ -0,0 +1,1 @@
+import{a as t,b as a,e as r,t as n,o}from"./entry.e1aa9ba1.js";const s={};function c(e,l){return o(),a("div",null,[r("pre",null,n(e.$route.path),1)])}const p=t(s,[["render",c]]);export{p as default};
