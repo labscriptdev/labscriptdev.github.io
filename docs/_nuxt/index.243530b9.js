f0731fd3133f3bb6fc8d5c80fa5e657ff51a49af
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-01-24 21:23:36 -0300

    build

--- docs/_nuxt/index.243530b9.js
+++
@@ -1,1 +0,0 @@
-import{a as e,l as o,o as t}from"./entry.65fd9d93.js";const n={mounted(){}};function r(_,a,c,s,l,p){return t(),o("div",null," brain ")}const d=e(n,[["render",r]]);export{d as default};
---
+++ docs/_nuxt/index.272916ba.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./layout.4da6076b.js";import{a,c as n,w as r,o as c,b as s,t as _}from"./entry.8121f650.js";const p={};function l(t,u){const e=o;return c(),n(e,null,{default:r(()=>[s("pre",null,_(t.$route.path),1)]),_:1})}const m=a(p,[["render",l]]);export{m as default};
