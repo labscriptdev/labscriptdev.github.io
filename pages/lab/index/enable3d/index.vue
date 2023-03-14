9c9a5538df4002797b1b92eb2ecfbed1c3a89dad
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-03-14 14:37:12 -0300

    wip

--- pages/lab/index/enable3d/index.vue
+++ pages/lab/index/enable3d/index.vue
@@ -1,28 +1,52 @@
 <template>
   <app-layout :container-card="false" container-width="100%">
-    <threejs-scene v-model="test" #default="{ game }">
-      <!-- <pre style="position:absolute; bottom:0; right:0;">{{ game.car.getSpeed() }}</pre> -->
-    </threejs-scene>
-    <a href="">Refresh</a>
-    <!-- <app-dd v-model="test"></app-dd> -->
+    <!-- <threejs-scene v-model="test" #default="{ game }" /> -->
     
-    <v-text-field v-model="test.name" />
-    <template v-if="test.track">
-      <!-- <div class="d-flex">
-        <v-text-field label="x" type="number" v-model.number="test.track.position.x" />
-        <v-text-field label="y" type="number" v-model.number="test.track.position.y" />
-        <v-text-field label="z" type="number" v-model.number="test.track.position.z" />
-      </div> -->
-      <!-- <pre>test.input: {{ test.input }}</pre> -->
-    </template>
+    <v-row>
+      <v-col cols="8">
+        <div ref="gameRef" style="height:200px; border:solid 1px red;"></div>
+        <pre>game: {{ game }}</pre>
+      </v-col>
+      <v-col cols="4">
+        <v-checkbox
+          v-model="game.debug"
+          v-bind="{
+            label: 'Debug',
+            hideDetails: true,
+          }"
+        />
+        <v-checkbox
+          v-model="game.gridHelper"
+          v-bind="{
+            label: 'Grid helper',
+            hideDetails: true,
+          }"
+        />
+        <v-checkbox
+          v-model="game.orbitControls"
+          v-bind="{
+            label: 'Orbit controls',
+            hideDetails: true,
+          }"
+        />
+        <v-text-field
+          v-model.number="game.fps"
+          v-bind="{
+            label: 'FPS',
+            type: 'number',
+            min: 1,
+            hideDetails: true,
+          }"
+        />
+      </v-col>
+    </v-row>
 
-    <!-- <div ref="game" id="game" style="width:100%; height:600px;"></div> -->
-    <!-- <pre>{{ game }}</pre> -->
+    <a href="">Refresh</a>
   </app-layout>
 </template>
 
 <script>
-  import Test from './Test';
+  // import Test from './Test';
 
   export default {
     meta: {
@@ -32,8 +56,124 @@
       description: 'Aplicação para gerar arquivos com tamanhos exatos',
     },
 
-    data: () => ({
-      test: Test,
-    }),
+    // data: () => ({
+    //   test: Test,
+    // }),
   };
+</script>
+
+<script setup>
+  import { ref, watch, onMounted, onUnmounted } from 'vue';
+  import { useEventListener } from '@vueuse/core';
+  import _ from 'lodash';
+  import { PhysicsLoader } from 'enable3d';
+  import * as THREE from 'three';
+
+  const useGame = (gameData={}) => {
+    gameData = _.merge({
+      name: 'no name',
+      fps: 60,
+      frame: 0,
+      width: 0,
+      height: 0,
+      debug: false,
+      gridHelper: false,
+      orbitControls: false,
+      scene: 0,
+      global: {},
+      prefabs: [],
+      scenes: [],
+      onSceneChange: () => {},
+    }, gameData);
+
+    gameData.scenes = gameData.scenes.map((scene, index) => {
+      return _.merge({
+        name: `scene-${index}`,
+        children: [],
+        onCreate: () => {},
+        onUpdate: () => {},
+        onProgress: () => {},
+        clock: false,
+        camera: false,
+        scene: false,
+        renderer: false,
+      }, scene);
+    });
+
+    const r = ref({
+      ...gameData,
+
+      init(elem) {
+        this._element = elem;
+        this.width = elem.offsetWidth;
+        this.height = elem.offsetHeight;
+        this.frame = 0;
+
+        useEventListener(window, 'resize', (ev) => {
+          this.width = elem.offsetWidth;
+          this.height = elem.offsetHeight;
+        });
+
+        const createTimeout = () => {
+          this._timeout = setTimeout(() => {
+            this.sceneUpdate();
+            createTimeout();
+          }, 1000/this.fps);
+        };
+
+        createTimeout();
+        this.sceneCreate();
+      },
+
+      sceneCreate() {
+        this._scene = this.scenes[ this.scene ] || this.scenes.filter(scene => scene.name==this.scene).at(0);
+        if (!this._scene) return;
+
+        this._scene.scene = new THREE.Scene();
+        this._scene.camera = new THREE.PerspectiveCamera(50, this.width / this.height, 1, 1000);
+        this._scene.clock = new THREE.Clock();
+        this._scene.renderer = new THREE.WebGLRenderer();
+        this._scene.renderer.setSize(this.width, this.height);
+        this._element.innerHTML = '';
+        this._element.appendChild(this._scene.renderer.domElement);
+      },
+
+      sceneUpdate() {
+        this.frame = this.frame + 1;
+        this._scene.renderer.render(this._scene.scene, this._scene.camera);
+        if (this.orbitControls) this.orbitControls.update();
+        if (this.options.debug) this.physics.updateDebugger();
+      },
+    });
+
+    onUnmounted(() => {
+      if (r.value && r.value._timeout) {
+        clearTimeout(r.value._timeout);
+      }
+    });
+
+    return r;
+  };
+
+  onMounted(() => {
+    PhysicsLoader('/assets/ammo', () => {
+      game.value.init(gameRef.value);
+    });
+  });
+
+  const gameRef = ref(null);
+  const game = useGame({
+    fps: 30,
+    orbitControls: true,
+    gridHelper: true,
+    scene: 'scene-0',
+    scenes: [
+      {
+        name: 'scene-0',
+      },
+      {
+        name: 'scene-1',
+      },
+    ],
+  });
 </script>
