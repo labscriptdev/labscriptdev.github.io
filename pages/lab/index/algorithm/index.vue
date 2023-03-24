128ae33b626e558bac2ddf15f5df29834f514a03
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-03-24 01:39:39 -0300

    Algorithm page

---
+++ pages/lab/index/algorithm/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <app-layout>
+    <nuxt-page :pages="pages" />
+
+    <template #drawer>
+      <v-list>
+        <v-list-item
+          v-for="p in pages"
+          :key="p.slug"
+          :title="p.name"
+          :prepend-icon="p.icon"
+          :to="`/lab/algorithm/${p.slug}`"
+          :exact="true"
+        ></v-list-item>
+      </v-list>
+    </template>
+  </app-layout>
+</template>
+
+<script>
+  export default {
+    meta: {
+      active: false,
+      icon: 'mdi-code-json',
+      name: 'Algorithms',
+      description: 'Types of algorithms',
+      source: 'https://github.com/labscriptdev/labscriptdev.github.io/tree/main/pages/lab/index/algorithm',
+    },
+
+    data() {
+      return {
+        pages: (() => {
+          let files = Object.entries(import.meta.glob('./index/**/*.vue', {
+            import: 'default',
+            eager: true,
+            // as: 'raw',
+          }));
+
+          return files
+            .filter(([path, file]) => !!file.meta)
+            .map(([path, file]) => {
+              return {
+                slug: path.split('/').at(-2).replace(/^index$/g, ''),
+                icon: null,
+                name: 'Default',
+                active: false,
+                order: 0,
+                ...(file.meta || {})
+              };
+            })
+            .sort((a, b) => {
+              if (a.order < b.order) return -1;
+              if (a.order > b.order) return 1;
+              return 0;
+            });
+        })(),
+      };
+    },
+  };
+</script>
---
+++ pages/lab/index/algorithm/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Choose one of the algorithms
+  </div>
+</template>
+
+<script>
+  export default {
+    meta: {
+      icon: null,
+      name: 'Home',
+      order: -1,
+    },
+
+    props: {
+      pages: {
+        type: Array,
+        default: () => ([]),
+      },
+    },
+  };
+</script>
