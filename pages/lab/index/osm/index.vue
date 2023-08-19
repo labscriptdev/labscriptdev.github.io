2bc4d201815914a74f7aa95de75c1ae0800fd05b
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-08-18 22:43:01 -0300

    map

--- pages/lab/index/osm/index.vue
+++ pages/lab/index/osm/index.vue
@@ -1,46 +1,43 @@
 <template>
   <app-layout>
-    <v-row no-gutters>
-      <v-col cols="6">
-        <l-map
-          ref="mapRef"
-          v-model:zoom="map.zoom"
-          :center="map.center"
-          style="height:400px;"
-          :useGlobalLeaflet="false"
-        >
-          <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base" name="OpenStreetMap" />
-          <l-marker :latLng="{ lat: map.center[0], lng: map.center[1] }" />
-          <l-rectangle
-            :bounds="map.rectangle.bounds"
-            ref="rectangleRef"
-          />
-        </l-map>
-      </v-col>
-
-      <v-col cols="6">
-        {{ `https://c.tile.openstreetmap.org/${map.zoom}/${map.xy[0]}/${map.xy[1]}.png` }} <br>
-        <a :href="map.osmURL" target="_blank">{{ map.osmURL }}</a><br>
-        <img :src="`https://c.tile.openstreetmap.org/${map.zoom}/${map.xy[0]}/${map.xy[1]}.png`" alt="">
-        <!-- <v-list>
-          <v-list-item v-for="e in map.osm.data.elements">
-            {{ e }}
-          </v-list-item>
-        </v-list>
-        <pre>{{ map.osm.data }}</pre> -->
-      </v-col>
-    </v-row>
-
-    <pre>map.rectangle.bounds: {{ map.rectangle.bounds }}</pre>
-    <pre>char: {{ char }}</pre>
-    <pre>map.osm: {{ map.osm }}</pre>
-    <!-- <pre>map: {{ map }}</pre> -->
+    <div ref="rootRef">
+      <v-row no-gutters>
+        <v-col cols="6">
+          <l-map
+            ref="mapRef"
+            v-model:zoom="map.zoom"
+            :center="map.center"
+            style="height:400px;"
+            :useGlobalLeaflet="false"
+          >
+            <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base" name="OpenStreetMap" />
+            <l-marker :latLng="{ lat: map.center[0], lng: map.center[1] }" />
+            <l-rectangle :bounds="map.minArea.bounds" ref="minAreaRef" />
+            <l-rectangle :bounds="map.maxArea.bounds" ref="maxAreaRef" />
+          </l-map>
+        </v-col>
+  
+        <v-col cols="6">
+          <div v-html="map.svg"></div>
+          <div style="font-family:monospace;">{{ map.svg }}</div>
+          <!-- <pre>{{ `viewBox="${map.maxArea.bounds[0][1]} ${map.maxArea.bounds[0][0]} ${map.maxArea.bounds[1][1]-map.maxArea.bounds[0][1]} ${map.maxArea.bounds[1][0]-map.maxArea.bounds[0][0]}"` }}</pre> -->
+          <!-- {{ `https://c.tile.openstreetmap.org/${map.zoom}/${map.xy[0]}/${map.xy[1]}.png` }} <br>
+          <a :href="map.osmURL" target="_blank">{{ map.osmURL }}</a><br>
+          <img :src="`https://c.tile.openstreetmap.org/${map.zoom}/${map.xy[0]}/${map.xy[1]}.png`" alt=""> -->
+        </v-col>
+      </v-row>
+  
+      <pre>char: {{ char }}</pre>
+      <pre>map.svg: {{ map.svg }}</pre>
+    </div>
   </app-layout>
 </template>
 
 <script setup>
+  import _ from 'lodash';
   import axios from 'axios';
-  import { reactive, ref, computed, onMounted } from 'vue';
+  import { reactive, ref, computed, onMounted, getCurrentInstance } from 'vue';
+  const { ctx } = getCurrentInstance();
 
   import 'leaflet/dist/leaflet.css';
   import { LMap, LTileLayer, LMarker, LIcon, LRectangle } from '@vue-leaflet/vue-leaflet';
@@ -49,21 +46,25 @@
 
   hotkeys('w, up', (ev) => {
     char.coords[0] += char.speed;
+    map.center[0] += char.speed;
     map.update();
   });
   
   hotkeys('s, down', (ev) => {
     char.coords[0] -= char.speed;
+    map.center[0] -= char.speed;
     map.update();
   });
 
   hotkeys('a, left', (ev) => {
     char.coords[1] -= char.speed;
+    map.center[1] -= char.speed;
     map.update();
   });
 
   hotkeys('d, right', (ev) => {
     char.coords[1] += char.speed;
+    map.center[1] += char.speed;
     map.update();
   });
   
@@ -77,101 +78,131 @@
     coords: [ 0, 0 ],
   });
 
-  const mapRef = ref(null);
-  const rectangleRef = ref(null);
-  const map = reactive({
-    xyid: '0-0',
-    zoom: 18,
-    center: [ 0, 0 ],
-    xy: [ 0, 0 ],
-    bboxArr: [ 0, 0, 0, 0 ],
-    bboxObj: { _southWest: { lat:0, lng:0 }, _northEast: { lat:0, lng:0 } },
-    rectangle: {
-      size: .001,
-      bounds: [
-        [ 0, 0 ],
-        [ 0, 0 ],
-      ],
-    },
-    osm: {
-      url: '',
-      data: { elements: [] },
-    },
-    xToLon(x) {
-      // console.log({ x }, this.zoom);
-      return this.zoom * x;
-    },
-    async update() {
-      const lat = char.coords[0];
-      const lng = char.coords[1];
+  const useMapData = (options={}) => {
 
-      map.center[0] = char.coords[0];
-      map.center[1] = char.coords[1];
+    options = _.merge({
+      zoom: 15,
+      center: [ 0, 0 ],
+    }, options);
 
-      (() => {
+    const r = reactive({
+      ...options,
+
+      minArea: {
+        size: .001,
+        bounds: [[0, 0], [0, 0]],
+      },
+      maxArea: {
+        size: .005,
+        bounds: [[0, 0], [0, 0]],
+      },
+      osm: {
+        loading: false,
+        data: { elements: [] },
+        to: false,
+        async load() {
+          if (r.osm.to) clearTimeout(r.osm.to);
+          r.osm.to = setTimeout(async () => {
+            const bbox = [ ...r.maxArea.bounds[0], ...r.maxArea.bounds[1] ];
+            r.osm.loading = true;
+            const { data } = await axios.get(`https://overpass-api.de/api/interpreter?data=[out:json];node(${bbox.join(',')});out;`);
+            r.osm.loading = false;
+            r.osm.data = data;
+          }, 500);
+        },
+      },
+
+      update() {
+        r.updateAreas();
+
+        getRef('minAreaRef', (comp) => {
+          comp.leafletObject.setBounds(r.minArea.bounds);
+        });
+
+        getRef('maxAreaRef', (comp) => {
+          comp.leafletObject.setBounds(r.maxArea.bounds);
+        });
+      },
+
+      updateAreas() {
+        if (
+          (
+            r.minArea.bounds[0][0]==0
+            && r.minArea.bounds[0][1]==0
+            && r.minArea.bounds[1][0]==0
+            && r.minArea.bounds[1][1]==0
+          )
+          || r.center[0] < r.minArea.bounds[0][0]
+          || r.center[0] > r.minArea.bounds[1][0]
+          || r.center[1] < r.minArea.bounds[0][1]
+          || r.center[1] > r.minArea.bounds[1][1]
+        ) {
+          r.minArea.bounds[0] = [ r.center[0]-r.minArea.size, r.center[1]-r.minArea.size ];
+          r.minArea.bounds[1] = [ r.center[0]+r.minArea.size, r.center[1]+r.minArea.size ];
+          r.maxArea.bounds[0] = [ r.center[0]-r.maxArea.size, r.center[1]-r.maxArea.size ];
+          r.maxArea.bounds[1] = [ r.center[0]+r.maxArea.size, r.center[1]+r.maxArea.size ];
+          r.osm.load();
+        }
+      },
+
+      coordsToMercador([ lat, lng ], zoom) {
         const R = 6378137,
           sphericalScale = 0.5 / (Math.PI * R),
           d = Math.PI / 180,
           max = 1 - 1E-15,
           sin = Math.max(Math.min(Math.sin(lat * d), max), -max),
-          scale = 256 * Math.pow(2, map.zoom);
+          scale = 256 * Math.pow(2, zoom);
   
-        map.xy[0] = R * lng * d;
-        map.xy[0] = scale * (sphericalScale * map.xy[0] + 0.5);
-        map.xy[0] = Math.floor(map.xy[0] / 256);
+        let x = R * lng * d;
+        x = scale * (sphericalScale * x + 0.5);
+        x = Math.floor(x / 256);
   
-        map.xy[1] = R * Math.log((1 + sin) / (1 - sin)) / 2;
-        map.xy[1] = scale * (-sphericalScale * map.xy[1] + 0.5);
-        map.xy[1] = Math.floor(map.xy[1] / 256);
-        
-        map.xyid = map.xy.join('-');
-      })();
+        let y = R * Math.log((1 + sin) / (1 - sin)) / 2;
+        y = scale * (-sphericalScale * y + 0.5);
+        y = Math.floor(y / 256);
 
+        return [ x, y, ];
+      },
 
-      if (mapRef.value && mapRef.value.leafletObject) {
-        const bbox = mapRef.value.leafletObject.getBounds();
-        map.bboxObj = bbox;
-        map.bboxArr = [ bbox._southWest.lat, bbox._southWest.lng, bbox._northEast.lat, bbox._northEast.lng ];
-        mapRef.value.leafletObject.panTo(map.center);
-      }
+      // mercator: computed(() => {
+      //   return r.coordsToMercador(r.center, r.zoom);
+      // }),
 
-      // Rectangle size
-      (async () => {
-        const { size, bounds } = map.rectangle;
-
-        const loadOsmData = async () => {
-          const bbox = [ ...bounds[0], ...bounds[1] ];
-          map.osm.url = `https://overpass-api.de/api/interpreter?data=[out:json];node(${bbox.join(',')});out;`;
-          const { data } = await axios.get(map.osm.url);
-          map.osm.data = data;
-        };
-
-        if ('[[0,0],[0,0]]'==JSON.stringify(bounds)) {
-          bounds[0] = [ map.center[0]-size, map.center[1]-size ];
-          bounds[1] = [ map.center[0]+size, map.center[1]+size ];
-          await loadOsmData();
+      svg: computed(() => {
+        const numberPositive = (n) => {
+          console.log(n);
+          return Math.max(n, n*-1);
         }
+        let lines = [`<svg width="400" height="400"`];
+        lines.push(`viewBox="${r.maxArea.bounds[0][1]} ${r.maxArea.bounds[0][0]} ${numberPositive(r.maxArea.bounds[0][1]-r.maxArea.bounds[1][1])} ${numberPositive(r.maxArea.bounds[0][0]-r.maxArea.bounds[1][0])}"`);
+        lines.push(`style="width:100%; border:solid 1px red;">`);
+        lines.push(`</svg>`);
 
-        if (
-          char.coords[0] < bounds[0][0]
-          || char.coords[0] > bounds[1][0]
-          || char.coords[1] < bounds[0][1]
-          || char.coords[1] > bounds[1][1]
-        ) {
-          bounds[0] = [ map.center[0]-size, map.center[1]-size ];
-          bounds[1] = [ map.center[0]+size, map.center[1]+size ];
-          await loadOsmData();
-        }
+        console.clear();
+        console.log(JSON.stringify(r.osm.data.elements, null, 2));
+        return lines.join('');
+      }),
+    });
 
-        // bounds[0] = [ map.center[0]-size, map.center[1]-size ];
-        // bounds[1] = [ map.center[0]+size, map.center[1]+size ];
+    watch([ r.center ], ([ center ]) => {
+      r.update();
+    });
 
-        if (rectangleRef.value) {
-          rectangleRef.value.leafletObject.setBounds(bounds);
-        }
-      })();
-    },
-  });
+    r.update();
+    return r;
+  };
+
+  const getRef = (name, callback) => {
+    const reff = ctx.$refs[name] || false;
+    if (reff && callback!==null && typeof callback=='function') {
+      callback(reff);
+    }
+    return reff;
+  };
+
+  const rootRef = ref(null);
+  const mapRef = ref(null);
+  const map = useMapData();
 
   onMounted(() => {
     navigator.geolocation.getCurrentPosition(
@@ -179,6 +210,9 @@
         char.coords[0] = geo.coords.latitude;
         char.coords[1] = geo.coords.longitude;
         map.update();
+
+        map.center[0] = geo.coords.latitude;
+        map.center[1] = geo.coords.longitude;
       },
       () => {}
     );
@@ -197,133 +231,3 @@
     },
   };
 </script>
-
-<!-- <template>
-  <app-layout>
-    <v-row no-gutters>
-      <v-col cols="6">
-        <l-map
-          v-model:tileLayer="osm.tileLayer"
-          v-model:zoom="osm.zoom"
-          v-model:center="osm.center"
-          v-model:bounds="osm.bounds"
-          @map:any="osm.onMapEvent($event)"
-          style="height:500px;"
-        ></l-map>
-        <v-select v-model="osm.tileLayer" :items="tileLayers" item-title="title" item-value="value" />
-      </v-col>
-
-      <v-col cols="6">
-        <threejs-scene v-model="scene" style="height:500px;" />
-      </v-col>
-    </v-row>
-  </app-layout>
-</template>
-
-<script>
-  import axios from 'axios';
-
-  export default {
-    meta: {
-      active: false,
-      icon: 'mdi-map',
-      name: 'OSM',
-      description: 'Open Street Map',
-      source: 'https://github.com/labscriptdev/labscriptdev.github.io/tree/main/pages/lab/index/osm',
-    },
-
-    data() {
-      return {
-        osm: new Osm(),
-        scene: new Scene(),
-
-        // https://leaflet-extras.github.io/leaflet-providers/preview/
-        tileLayers: [
-          {
-            title: 'Default',
-            value: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          },
-          {
-            title: 'OSM Humanitarian',
-            value: 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
-          },
-          {
-            title: 'World Imagery',
-            value: 'http://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-          },
-          {
-            title: 'World Topo Map',
-            value: 'http://services.arcgisonline.com/arcgis/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
-          },
-          {
-            title: 'Dark Matter',
-            value: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
-          },
-          {
-            title: 'Toner',
-            value: 'http://tile.stamen.com/toner/{z}/{x}/{y}.png',
-          },
-          {
-            title: 'Watercolor',
-            value: 'http://tile.stamen.com/watercolor/{z}/{x}/{y}.png',
-          },
-          {
-            title: 'aaa',
-            value: 'https://tile.mapzen.com/mapzen/terrain/v1/normal/{z}/{x}/{y}.png',
-          },
-        ],
-      };
-    },
-  };
-
-  class Osm {
-    constructor() {
-      this.tileLayer = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-      this.zoom = 18;
-      this.center = [-19.920421630138943, -43.936042785661876];
-      this.bounds = [0, 0, 0, 0];
-      this.xy = [0, 0];
-      this.xyUrl = 'https://tile.openstreetmap.org/18/99080/145879.png';
-      this.osmBuildings = {};
-      this.coords = { lat: 0, lng: 0 };
-      this.osmUrl = false;
-      this.osmData = {elements:[]};
-      this._t = {};
-    }
-
-    onMapEvent(ev) {
-      this.osmUrl = ev.osmUrl;
-      this.bounds = ev.bounds;
-      this.xy = ev.xy;
-      this.xyUrl = `https://tile.openstreetmap.org/${this.zoom}/${this.xy.y}/${this.xy.x}.png`;
-      this.debounce(1000, async () => {
-        
-        if (this.zoom>=15 && this.zoom<=16) {
-          // https://c.data.osmbuildings.org/0.2/dixw8kmb/tile/15/17605/10745.json
-          const { data: osmBuildings } = await axios.get(`https://c.data.osmbuildings.org/0.2/dixw8kmb/tile/${this.zoom}/${this.xy.y}/${this.xy.x}.json`);
-          this.osmBuildings = osmBuildings;
-        }
-
-        if (this.zoom==18) {
-          const { data } = await axios.get(this.osmUrl);
-          this.osmData = data;
-        }
-      });
-    }
-
-    debounce(wait, func) {
-      const k = btoa(func.toString());
-      if (this._t[k]) clearTimeout(this._t[k]);
-      this._t[k] = setTimeout(func, wait);
-    }
-  };
-
-  class Scene {
-    constructor() {
-      this.options = {
-        gridHelper: true,
-        orbitControls: true,
-      };
-    }
-  };
-</script> -->
