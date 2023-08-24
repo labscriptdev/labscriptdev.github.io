349b7c7c7482d089df83fe350ddb207112b7a927
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-08-24 10:03:05 -0300

    fps test

---
+++ pages/lab/index/fps-test/index/click-speed/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    page-1
+  </div>
+</template>
+
+<script>
+  export default {
+    meta: {
+      icon: false,
+      name: 'Velocidade de clique',
+    },
+  };
+</script>
---
+++ pages/lab/index/fps-test/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    Selecione um teste
+  </div>
+</template>
+
+<script>
+  export default {
+    meta: {
+      icon: false,
+      name: 'Home',
+    },
+
+    data() {
+      return {
+        test: true,
+      };
+    },
+
+    methods: {
+      say(text) {
+        console.log(text);
+      },
+    },
+  };
+</script>
