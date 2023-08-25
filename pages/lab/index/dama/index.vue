6cb0ddc887bff5e68d37f6ae060edd8d06e1a500
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-08-25 16:21:11 -0300

    checkers

---
+++ pages/lab/index/dama/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <app-layout>
+    <pre>checkers: {{ checkers }}</pre>
+  </app-layout>
+</template>
+
+<script setup>
+  import { reactive } from 'vue';
+
+  const useCheckers = () => {
+    const r = reactive({
+      turn: 1,
+      positions: Array(8).fill().map((n, i) => Array(8).fill().map((n, i) => null)),
+    });
+    return r;
+  };
+
+  const checkers = useCheckers();
+</script>
+
+<script>
+  export default {
+    meta: {
+      active: false,
+      icon: 'arcticons:checkers',
+      name: 'Dama',
+      description: 'Base file structure',
+      source: 'https://github.com/labscriptdev/labscriptdev.github.io/tree/main/pages/lab/index/base',
+    },
+  };
+</script>
