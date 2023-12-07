9d359b127647415b87be42bb214274fa8394e8be
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-12-06 22:33:01 -0300

    List subpages

--- nuxt3/pages/lab/index/_base/index.vue
+++ nuxt3/pages/lab/index/_base/index.vue
@@ -4,56 +4,59 @@
 
     <template #drawer>
       <v-list>
-        <v-list-item
-          v-for="p in pages"
-          :key="p.slug"
-          :title="p.name"
-          :prepend-icon="p.icon"
-          :to="`/lab/base/${p.slug}`"
-        ></v-list-item>
+        <template v-for="p in pages">
+          <v-list-item :title="p.name" :prepend-icon="p.icon" :to="p.url" />
+        </template>
       </v-list>
     </template>
   </app-layout>
 </template>
 
 <script>
-  export default {
-    meta: {
-      active: false,
-      icon: 'mdi-circle',
-      name: 'Base',
-      description: 'Base file structure',
-      source: 'https://github.com/labscriptdev/labscriptdev.github.io/tree/main/pages/lab/index/base',
-    },
+export default {
+  meta: {
+    active: false,
+    icon: "mdi-circle",
+    name: "Base",
+    baseUrl: "/lab/_base",
+    description: "Base file structure",
+    source:
+      "https://github.com/labscriptdev/labscriptdev.github.io/tree/main/pages/lab/index/base",
+  },
 
-    data() {
-      return {
-        pages: (() => {
-          let files = Object.entries(import.meta.glob('./index/**/*.vue', {
-            import: 'default',
+  data() {
+    return {
+      pages: (() => {
+        let files = Object.entries(
+          import.meta.glob("./index/**/*.vue", {
+            import: "default",
             eager: true,
             // as: 'raw',
-          }));
+          })
+        );
 
-          return files
-            .filter(([path, file]) => !!file.meta)
-            .map(([path, file]) => {
-              return {
-                slug: path.split('/').at(-2).replace(/^index$/g, ''),
-                icon: 'mdi-circle',
-                name: 'Default',
-                active: false,
-                order: 0,
-                ...(file.meta || {})
-              };
-            })
-            .sort((a, b) => {
-              if (a.order < b.order) return -1;
-              if (a.order > b.order) return 1;
-              return 0;
-            });
-        })(),
-      };
-    },
-  };
+        return files
+          .filter(([path, file]) => !!file.meta)
+          .map(([path, file]) => {
+            const url =
+              this.$options.meta.baseUrl +
+              path.replace("/index.vue", "").replace("./index", "");
+            return {
+              url,
+              icon: "mdi-circle",
+              name: "Default",
+              active: false,
+              order: 0,
+              ...(file.meta || {}),
+            };
+          })
+          .sort((a, b) => {
+            if (a.order < b.order) return -1;
+            if (a.order > b.order) return 1;
+            return 0;
+          });
+      })(),
+    };
+  },
+};
 </script>
